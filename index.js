fa2233089372b3f9deae548cee02d655429b8705
Hani <md.hani@hotmail.com>
2022-06-09 14:54:02 +0300

    edited

--- index.js
+++ index.js
@@ -1,90 +1,136 @@
-const myEach = (testArr, alert) => {
-    for (let i = 0; i < testArr.length; i++) {
-        alert(testArr[i]);
-        return testArr;
+
+const standardizeInput = function(collection) {
+    return (collection instanceof Array) ? collection.slice() : Object.values(collection);
+  }
+  
+  const myEach = function(collection, callback) {
+    const newCollection = standardizeInput(collection);
+  
+    for (let idx = 0; idx < newCollection.length; idx++) {
+      callback(newCollection[idx]);
     }
+  
+    return collection;
+  }
+  
+  const myMap = function(collection, callback) {
+    const newCollection = standardizeInput(collection);
+  
+    const newArr = [];
+  
+    for (let idx = 0; idx < newCollection.length; idx++) {
+      newArr.push(callback(newCollection[idx]));
     }
-    const myMap = (testArr, callback) => {
-        let newArray = [];
-        for (let i = 0; i < testArr.length; i++) {
-            newArray.push(callback(testArr[i]));
+  
+    return newArr;
+  }
+  
+  const myReduce = function(collection, callback, acc) {
+    let newCollection = standardizeInput(collection);
+    if (!acc) {
+      acc = newCollection[0];
+      newCollection = newCollection.slice(1);
+    }
+  
+    const len = newCollection.length;
+  
+    for (let i = 0; i < len; i++) {
+      acc = callback(acc, newCollection[i], newCollection);
+    }
+    return acc;
+  }
+  
+  const myFind = function(collection, predicate) {
+    const newCollection = standardizeInput(collection);
+  
+    for (let idx = 0; idx < newCollection.length; idx++) {
+      if (predicate(newCollection[idx])) return newCollection[idx];
+    }
+  
+    return undefined;
+  }
+  
+  const myFilter = function(collection, predicate) {
+    const newCollection = standardizeInput(collection);
+  
+    const newArr = [];
+  
+    for (let idx = 0; idx < newCollection.length; idx++) {
+      if (predicate(newCollection[idx])) newArr.push(newCollection[idx]);
+    }
+  
+    return newArr;
+  }
+  
+  const mySize = function(collection) {
+    const newCollection = standardizeInput(collection);
+    return newCollection.length;
+  }
+  
+
+  
+  const myFirst = function(arr, stop=false) {
+    return (stop) ? arr.slice(0, stop) : arr[0];
+  }
+  
+  const myLast = function(arr, start=false) {
+    return (start) ? arr.slice(arr.length-start, arr.length) : arr[arr.length-1];
+  }
+  
+  const mySortBy = function(arr, callback) {
+    const newArr = [...arr];
+    return newArr.sort(function(a, b) {
+      if (callback(a) > callback(b)) {
+        return 1;
+      } else if (callback(b) > callback(a)) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+  }
+  
+  const unpack = function(receiver, arr) {
+    for (let val of arr) {
+      receiver.push(val);
+    }
+  }
+  
+
+  const myFlatten = function(collection, shallow, newArr=[]) {
+    if (shallow) {
+      for (let val of collection) {
+        Array.isArray(val) ? unpack(newArr, val) : newArr.push(val);
+      }
+    } else {
+
+      for (let val of collection) {
+        if (Array.isArray(val)) {
+
+          myFlatten(val, false, newArr);
+        } else {
+          newArr.push(val);
         }
-        return newArray;
+      }
     }
-    const myReduce = (array, callback, initialValue) => {
-        let accumulator = initialValue;
-        for (let i = 0; i < array.length; i++) {
-            accumulator = callback(accumulator, array[i]);
-        }
-        return accumulator;
+    return newArr;
+  }
+  
+
+  
+  const myKeys = function(obj) {
+    const keys = [];
+    for (let key in obj){
+      keys.push(key);
     }
-    const myFilter = (array, callback) => {
-        let newArray = [];
-        for (let i = 0; i < array.length; i++) {
-            if (callback(array[i])) {
-                newArray.push(array[i]);
-            }
-        }
-        return newArray;
+    return keys;
+  }
+  
+  const myValues = function(obj) {
+    const values = [];
+    for (let key in obj){
+      values.push(obj[key]);
     }
-    const myFind = (array, callback) => {
-        for (let i = 0; i < array.length; i++) {
-            if (callback(array[i])) {
-                return array[i];
-            }
-        }
-    }
-    const mySome = (array, callback) => {
-        for (let i = 0; i < array.length; i++) {
-            if (callback(array[i])) {
-                return true;
-            }
-        }
-        return false;
-    }
-    const myEvery = (array, callback) => {
-        for (let i = 0; i < array.length; i++) {
-            if (!callback(array[i])) {
-                return false;
-            }
-        }
-        return true;
-    }
-    const myFlatten = (array) => {
-        let newArray = [];
-        for (let i = 0; i < array.length; i++) {
-            if (Array.isArray(array[i])) {
-                newArray = newArray.concat(myFlatten(array[i]));
-            } else {
-                newArray.push(array[i]);
-            }
-        }
-        return newArray;
-    }
-    const mySize = (array, size) => {
-        let newArray = [];
-        for (let i = 0; i < array.length; i += size) {
-            newArray.push(array.slice(i, i + size));
-        }
-        return newArray;
-    }
-    const myFirst = (arr) => {
-        return arr[0];
-    }
-    const myLast = (arr) => {
-        return arr[arr.length - 1];
-    }
-    const myKeys = (obj) => {
-        let newArray = [];
-        for (let key in obj) {
-            newArray.push(key);
-        }
-        return newArray;
-    }
-    const myValues = (obj) => {
-        let newArray = [];
-        for (let key in obj) {
-            newArray.push(obj[key]);
-        }
-        return newArray;
-    }
+    return values;
+  
+  }
